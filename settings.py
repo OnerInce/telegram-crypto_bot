9326359ad06a4c9f86e6650d7fbdf36a0afe9f70
OnerInce <onerince@hotmail.com>
2021-05-27 21:55:38 +0300

    change settings

--- settings.py
+++ settings.py
@@ -1,19 +1,19 @@
+import os
+from dotenv import load_dotenv
+
 """
 Define constants to use token properly
 """
+
+load_dotenv()  # take environment variables from .env.
 
 constants = {
     # List of the exchange APIs
     "API_URLS": ['https://www.paribu.com/ticker', 'https://api.btcturk.com/api/v2/ticker'],
     # Telegram bot token
-    "BOT_TOKEN": "",
+    "BOT_TOKEN": os.environ['BOT_TOKEN'],
     # SQLite database's file name
-    "DB_FILE_NAME": "",
+    "DB_FILE_NAME": "coindb.sqlite",
     # CoinMarketCap API KEY
-    "CMC_API_KEY": '',
+    "CMC_API_KEY": os.environ['CMC_KEY'],
 }
-
-try:
-    from local_settings import constants
-except ImportError:
-    pass
