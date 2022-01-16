1fe4b02df79369ea5736e2eb5cf7c2a315f7cb57
Oner Ince <onerince@protonmail.com>
2022-01-16 17:50:39 +0300

    add unit test

---
+++ tests/test_apis.py
@@ -0,0 +1,19 @@
+import requests
+
+from settings import API_URLS
+from src.cmc_data import get_coin_name_mappings
+
+
+def test_cmc_api():
+    coin_name_dict = get_coin_name_mappings()
+
+    assert type(coin_name_dict) == dict
+    assert len(coin_name_dict) > 1000
+
+
+def test_exchange_apis():
+    for api in API_URLS:
+        r = requests.get(api)
+
+        assert r.status_code == 200
+        assert type(r.json()) == dict
