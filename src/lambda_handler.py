2669cd0a6b6d5e825fecae668a15031c7c33dcfb
Oner Ince <onerince@protonmail.com>
2022-01-16 18:04:46 +0300

    remove lambda file

---
+++ src/lambda_handler.py
@@ -0,0 +1,3 @@
+# https://api.telegram.org/botREDACTED/setWebhook?url=https://t0dn7lhk34.execute-api.us-east-2.amazonaws.com/beta
+# https://api.telegram.org/botREDACTED/deleteWebhook
+# https://api.telegram.org/botREDACTED/getUpdates
