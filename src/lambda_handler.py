b8ceffabba265a120830618c6a839cc3343334ff
OnerInce <onerince@hotmail.com>
2022-01-20 17:50:33 +0300

    remove handler

--- src/lambda_handler.py
+++
@@ -1,3 +0,0 @@
-# https://api.telegram.org/botREDACTED/setWebhook?url=https://t0dn7lhk34.execute-api.us-east-2.amazonaws.com/beta
-# https://api.telegram.org/botREDACTED/deleteWebhook
-# https://api.telegram.org/botREDACTED/getUpdates
