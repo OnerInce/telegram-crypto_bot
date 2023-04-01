560296f9d164ffba20f65c1dc67f4f31c0e761cb
Oner Ince <oner.ince@nielsen.com>
2023-04-01 18:15:59 +0200

    remove broken paribu endpoint

--- src/settings.py
+++ src/settings.py
@@ -10,7 +10,7 @@ load_dotenv()  # take environment variables from .env
 
 
 # List of the exchange APIs
-API_URLS = ['https://v3.paribu.com/app/initials', 'https://api.btcturk.com/api/v2/ticker']
+API_URLS = ['https://api.btcturk.com/api/v2/ticker']
 
 # CoinMarketCap API KEY
 CMC_API_KEY = os.environ["CMC_KEY"]
