8849203f344ee769fd0124da4460f8aa7d143798
OnerInce <onerince@protonmail.com>
2025-07-10 21:39:47 +0200

    remove everything related to cmc

--- tests/test_apis.py
+++ tests/test_apis.py
@@ -1,18 +1,9 @@
 import requests
 from dotenv import load_dotenv
 
-from src.cmc_data import get_coin_name_mappings
 from src.settings import API_URLS
 
 load_dotenv()
-
-
-def test_cmc_api():
-    coin_name_dict = get_coin_name_mappings()
-
-    assert type(coin_name_dict) == dict
-    assert len(coin_name_dict) > 1000
-
 
 def test_exchange_apis():
     for api in API_URLS:
